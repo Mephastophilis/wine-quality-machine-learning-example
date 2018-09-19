6205e29f018e12a8a938c0cca5ebfaf666de2cf5
Bryan Quigley <bquigley@tulane.edu>
2018-09-19 18:58:16 -0500

    linear regression model working red wine

--- wine-quality-regression.py
+++ wine-quality-regression.py
@@ -7,13 +7,18 @@ This is a temporary script file.
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import numpy as np
+from sklearn.linear_model import LinearRegression
 
 reds = pd.read_csv('winequality-red.csv', sep=';')
 X = reds.values[:, :11]
 y = reds.values[:, 11]
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=98)
 
-X_means = np.mean(X, axis =0)
-X_train_means = np.mean(X_train, axis=0)
-X_test_means = np.mean(X_test, axis=0)
+lr = LinearRegression(normalize=True)
+
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+
+error = sum(abs(y_pred - y_test))
+
