9733daecbe4cf698c5169e97b97b51f13b0c2d98
Bryan Quigley <bquigley@tulane.edu>
2018-09-20 00:03:56 -0500

    added in regularized linear regression

--- wine-quality-regression.py
+++ wine-quality-regression.py
@@ -7,7 +7,8 @@ This is a temporary script file.
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, RidgeCV
+from sklearn.metrics import accuracy_score
 
 reds = pd.read_csv('winequality-red.csv', sep=';')
 X = reds.values[:, :11]
@@ -20,5 +21,13 @@ lr.fit(X_train, y_train)
 
 y_pred = lr.predict(X_test)
 
-error = sum(abs(y_pred - y_test))
+for x in xrange(len(y_pred)):
+    y_pred[x] = round(y_pred[x])
 
+print(accuracy_score(y_test, y_pred))
+
+
+reg = RidgeCV(alphas=[0.1, 1.0, 10.0])
+reg.fit(X, y, [0, .1, 1])       
+RidgeCV(alphas=[0.1, 1.0, 10.0], cv=None, fit_intercept=True, scoring=None, normalize=False)
+reg.alpha_
