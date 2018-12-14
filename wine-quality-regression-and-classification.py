33df53a091709eba3a10c7ae29c439d738d6162d
Bryan Quigley <bquigley@tulane.edu>
2018-12-14 15:05:44 -0600

    neural net included now

---
+++ wine-quality-regression-and-classification.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+wine quality regression and classification
+
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, RidgeCV
+import sklearn.metrics
+from sklearn.svm import SVC
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+import matplotlib.gridspec as gridspec
+
+def wine_data_analyzer(winedata, winetype):
+
+    df = pd.read_csv(winedata, sep=';')
+    df=df.rename(columns={'alcohol':'alcohol %'})
+    X = df.values[:, :11]
+    y = df.values[:, 11]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=30, test_size=0.15)
+
+    lr = LinearRegression(normalize=True)
+    lr.fit(X_train, y_train)
+    y_pred_lr = lr.predict(X_test)
+    print('Mean aboslute error for linear regression = ' + str(sklearn.metrics.mean_absolute_error(y_test, y_pred_lr)))
+    print('Mean squared error for linear regression = ' + str(sklearn.metrics.mean_squared_error(y_test, y_pred_lr)))
+    print('R^2 score for linear regression = ' + str(sklearn.metrics.r2_score(y_test, y_pred_lr)) + '\n')
+
+    alphas_list = np.arange(0.001,1,0.001)
+    reg = RidgeCV(alphas=alphas_list, cv=None, fit_intercept=True, scoring=None, normalize=True)
+    reg.fit(X_train, y_train)       
+    print('Alpha for regularized linear regression = ' + str(reg.alpha_))
+    y_pred_reg = reg.predict(X_test)
+    print('Mean aboslute error for regularized linear regression = ' + str(sklearn.metrics.mean_absolute_error(y_test, y_pred_reg)))
+    print('Mean squared error for regularized linear regression = ' + str(sklearn.metrics.mean_squared_error(y_test, y_pred_reg)))
+    print('R^2 score for regularized linear regression = ' + str(sklearn.metrics.r2_score(y_test, y_pred_reg)) + '\n')
+    
+    print('SVC fit with linear kernel')
+    svc = SVC(kernel='linear')
+    svc.fit(X_train, y_train)
+    y_pred_svc = svc.predict(X_test)
+    print(sklearn.metrics.classification_report(y_test, y_pred_svc))
+    
+    print('SVC fit with radial basis function kernel')
+    svc_g = SVC(kernel='rbf')
+    svc_g.fit(X_train, y_train)
+    y_pred_svc_g = svc_g.predict(X_test)
+    print(sklearn.metrics.classification_report(y_test, y_pred_svc_g))
+    
+    linear_contributions = abs(lr.coef_*(np.max(X, axis=0)-np.min(X, axis=0))*np.mean(X, axis=0))
+    most_relevant_feature_lr = np.where(abs(linear_contributions)==max(abs(linear_contributions)))[0][0]
+    linear_contributions_2 = np.delete(linear_contributions, np.where(linear_contributions==max(linear_contributions)))
+    sec_most_relevant_feature_lr = np.where(abs(linear_contributions_2)==max(abs(linear_contributions_2)))[0][0]+1
+    
+    figure = plt.figure(figsize=(8, 3.5),dpi=200)
+    gs  = gridspec.GridSpec(1, 2,  wspace=0.4, hspace=0.2)
+    a = figure.add_subplot(gs[0, 0:1])
+    a.scatter(X[:,most_relevant_feature_lr], y)
+    plt.xlabel(list(df.columns.values)[most_relevant_feature_lr].capitalize())
+    plt.ylabel(list(df.columns.values)[11].capitalize())
+    plt.title(winetype.capitalize() + ' Wine Quality vs. ' + str(list(df.columns.values)[most_relevant_feature_lr]).capitalize())
+    m1 = lr.coef_[most_relevant_feature_lr]*np.mean(X, axis=0)[most_relevant_feature_lr]
+    b1 = np.median(y)-m1*np.mean(X[y==np.median(y)], axis=0)[most_relevant_feature_lr]
+    x1_1 = (min(y)-b1)/m1
+    x2_1 = (max(y)-b1)/m1
+    x_plot=np.arange(min(x1_1, x2_1), max(x1_1, x2_1), abs(x1_1-x2_1)/100)
+    y_plot=x_plot*m1+b1
+    a.plot(x_plot,y_plot)
+   
+    a = figure.add_subplot(gs[0, 1:2])
+    a.scatter(X[:,sec_most_relevant_feature_lr], y)
+    plt.xlabel(list(df.columns.values)[sec_most_relevant_feature_lr].capitalize())
+    plt.ylabel(list(df.columns.values)[11].capitalize())
+    plt.title(winetype.capitalize() + ' Wine Quality vs. ' + str(list(df.columns.values)[sec_most_relevant_feature_lr]).capitalize())
+    m2 = lr.coef_[sec_most_relevant_feature_lr]*np.mean(X, axis=0)[sec_most_relevant_feature_lr]
+    b2 = np.median(y)-m2*np.mean(X[y==np.median(y)], axis=0)[sec_most_relevant_feature_lr]
+    x1_2 = (min(y)-b2)/m2
+    x2_2 = (max(y)-b2)/m2
+    x_plot=np.arange(min(x1_2, x2_2), max(x1_2, x2_2), abs(x1_2-x2_2)/100)
+    y_plot=x_plot*m2+b2
+    a.plot(x_plot,y_plot)
+    
+    plt.savefig(winetype + '_wine_plot.png')
+
+
+def wine_neural_net(winedata, winetype, alpha_1, hiddenlayersize):
+    
+    df = pd.read_csv(winedata, sep=';')
+    df=df.rename(columns={'alcohol':'alcohol %'})
+    X = df.values[:, :11]
+    y = df.values[:, 11]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=30, test_size=0.25)
+    
+    clf = MLPClassifier(solver='lbfgs', alpha=alpha_1, hidden_layer_sizes=hiddenlayersize, random_state=1)
+    clf.fit(X, y)
+    
+    y_pred_MLPC = clf.predict(X_test)
+    print('F1 score: ' + str(sklearn.metrics.f1_score(y_test, y_pred_MLPC,average='weighted')))
+    return sklearn.metrics.classification_report(y_test, y_pred_MLPC), sklearn.metrics.f1_score(y_test, y_pred_MLPC,average='weighted'), hiddenlayersize
+
+    
+    
+
+#wine_data_analyzer('winequality-red.csv', 'red')
+#wine_data_analyzer('winequality-white.csv', 'white')
+
+wine_datas = [('winequality-red.csv', 'red'), ('winequality-white.csv', 'white')]
+
+for wine in wine_datas:
+    wine_data_analyzer(wine[0], wine[1])
+    
+    hidden_layers_test = [(5, 2), (5, 2, 2), (4, 3, 3), (6, 6, 6), (5, 2, 2, 2), (8, 6, 4, 2), (4, 4, 4), (3, 3, 3, 3), (7, 3, 3) ]
+    results = []
+    for hiddenlayer in hidden_layers_test:
+        results.append(wine_neural_net(wine[0], wine[1], 1e-5, hiddenlayer))        
+    best_result_hl = max([x[1] for x in results])
+       
+    for x in results:
+        if x[1] == best_result_hl:
+            print(wine[1] + ' wine \n' + 'Best hidden layer size is: ' + str(x[2]) + ' which achieves F1 score of: ' + str(round(x[1], 4)) + '.')
+            print(x[0])
